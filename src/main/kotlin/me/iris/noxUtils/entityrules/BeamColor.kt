070978e332e905b8fb05d63f1d0d19ab33cf972d
Blockybomb <118032108+BlockybombDEV@users.noreply.github.com>
2024-10-20 19:25:46 +0200

    Make BeamColor#setBeamColor and BeamColor#resetBeamColor public

--- src/main/kotlin/me/iris/noxUtils/entityrules/BeamColor.kt
+++ src/main/kotlin/me/iris/noxUtils/entityrules/BeamColor.kt
@@ -3,19 +3,13 @@ package me.iris.noxUtils.entityrules
 import com.noxcrew.noxesium.api.protocol.rule.EntityRuleIndices
 import com.noxcrew.noxesium.paper.api.rule.RemoteServerRule
 import me.iris.noxUtils.NoxUtils
-import me.iris.noxUtils.NoxUtils.Companion.instance
-import org.bukkit.Bukkit
-import org.bukkit.Location
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.Guardian
-import org.bukkit.entity.LivingEntity
 import java.awt.Color
 import java.util.*
 
 public class BeamColor {
 
     private val entityManager = NoxUtils.entityRuleManager
-    private val noxesiumManager = NoxUtils.noxesiumManager
 
     // Preset colors for Skript
     public companion object {
@@ -30,34 +24,14 @@ public class BeamColor {
         public val PURPLE: Optional<Color> = Optional.of(Color(100, 0, 200))
         public val PINK: Optional<Color> = Optional.of(Color.MAGENTA)
         public val WHITE: Optional<Color> = Optional.of(Color.WHITE)
-    }
-
-
-    /**
-     * Spawns a Guardian at [location] with a [color] for [duration] targeting [target]
-     */
-    public fun spawnGuardian(location: Location, target: LivingEntity, color: Optional<Color>, duration: Long) {
-        val world = location.world
-        val guardian = world.spawnEntity(location, EntityType.GUARDIAN) as Guardian
-        guardian.setGravity(false)
-        guardian.isInvisible = true
-        guardian.isInvulnerable = true
-        guardian.isPersistent = true
-        guardian.isSilent = true
-        guardian.setAI(false)
-        guardian.target = target
-        guardian.setLaser(true)
-        setBeamColor(guardian, color)
-        Bukkit.getScheduler().runTaskLater(instance, Runnable {
-            if (!guardian.isDead) {
-                resetBeamColor(guardian)
-                guardian.remove()
-            }
-        }, duration)
+        public val BLACK: Optional<Color> = Optional.of(Color.BLACK)
+        public val GRAY: Optional<Color> = Optional.of(Color.GRAY)
+        public val LIGHT_GRAY: Optional<Color> = Optional.of(Color.LIGHT_GRAY)
+        public val DARK_GRAY: Optional<Color> = Optional.of(Color.DARK_GRAY)
     }
 
     // Will be updated to support minecraft:end_crystal once Paper 1.21.2 comes out
-    private fun setBeamColor(guardian: Guardian, color: Optional<Color>) {
+    public fun setBeamColor(guardian: Guardian, color: Optional<Color>) {
         // Set the beam color
         var rule: RemoteServerRule<Any>? = entityManager.getEntityRule(guardian, EntityRuleIndices.BEAM_COLOR)
         rule!!.setValue(color)
@@ -67,7 +41,7 @@ public class BeamColor {
         rule!!.setValue(true)
     }
 
-    private fun resetBeamColor(guardian: Guardian) {
+    public fun resetBeamColor(guardian: Guardian) {
         val rule: RemoteServerRule<Any>? = entityManager.getEntityRule(guardian, EntityRuleIndices.BEAM_COLOR)
         rule!!.reset()
     }
